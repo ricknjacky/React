a2d7a0148c5f4e69262b98791a9201e9c4393c40
ricknjacky <jaikishanbrijwani@gmail.com>
2020-09-29 23:31:37 +0530

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
-import './App.css';
+import styled from 'styled-components';
 import Person from './Person/Person';
+import './App.css';
+
+const StyledButton = styled.button`
+      background-color: ${props=> props.alt?'red':'green'};
+      color: white;
+      font: inherit;
+      border: 1px solid blue;
+      padding: 8px;
+      cursor: pointer;
+      
+      &:hover {
+        background-color: ${props=>props.alt?'salmon':'lightgreen'};
+        color: black; 
+`;
+
 
 class App extends Component {
   state = {
@@ -45,18 +60,11 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
 
     let persons = null;
 
     if (this.state.showPersons) {
-      persons = (
+      persons = ( 
         <div>
           {this.state.persons.map((person, index) => {
             return <Person
@@ -69,19 +77,27 @@ class App extends Component {
         </div>
       );
     }
+    
+    let classes = [];
+    if(this.state.persons.length<=2){
+      classes.push('red');//classes= [red]
+    }
+    if(this.state.persons.length<=1){
+      classes.push('bold');//classes = [red, bold]
+    }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <p className={classes.join(' ')} >This is really working!</p>
+        <StyledButton alt ={this.state.showPersons}
+          onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
         {persons}
       </div>
+      
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default App; 
